034b16191d97337c4376eb8733269a46d1c0dbe8
yokarakPC <Ande67f3@easj.edu.dk>
2020-01-06 16:09:57 +0100

    made the initial names of different instancevalues to - Levels - Player

--- Src/Game/Levels.java
+++ Src/Game/Levels.java
@@ -2,14 +2,17 @@ package Game;
 
 public class Levels {
 
-        String levelName;
-        int levelNumber;
+        private String levelName;
+        private int levelNumber;
 
-        int numberOfAdversaries; //the mininum number of needed to encounter - maybe a "bonus boss"
-        int difficulty; // 1 easy, 2 little harder 3 medium and so on
+        private int numberOfAdversaries; //the mininum number of needed to encounter - maybe a "bonus boss"
+        private int difficulty; // 1 easy, 2 little harder 3 medium and so on
 
 
         final double chanceOfBossEncounter=0.05; // 5%, times levelNumber so chances will be higher further one
+
+
+
 
 
 
--- Src/Game/Player.java
+++ Src/Game/Player.java
@@ -4,6 +4,13 @@ public class Player {
     // the class where all instance variables and methods for the player class will be located here
     //included all the different starter bonuses
 
+    private int health;
+    private double chanceOfKilling; // these four is instead of the "old fashioned" damage :D
+    private double chanceOfInjuring;
+    private double chanceOfSubduing;
+    private double chanceOfWinningOver;
+
+
 
 
 
