ae2ad24c1b7eeaf590461a0867eb16eb5adcd84c
yokarakPC <Ande67f3@easj.edu.dk>
2020-01-06 21:01:15 +0100

    Calculation.java made made calcalation for killing and injury tested killing in main testing injury now in main

--- Src/Game/Calculation.java
+++ Src/Game/Calculation.java
@@ -2,14 +2,50 @@ package Game;
 
 import java.util.Random;
 
-public class Calculation {
+public class Calculation extends Player {
     // this will be the where all the calculation will be made
-    Random random = new Random();
 
 
-    public static int generateRandomInt (int upperRange) {
+    public static double generateRandomInt() {
         Random random = new Random();
-        return random.nextInt(upperRange);
+        double genResult = random.nextDouble();
+        return genResult;
     }
 
+    public static double calculateKilling(double chanceNumber, double standardValue, double generatedNumber) {
+        double preResult = generatedNumber / standardValue;
+        // System.out.println("generated: " + generatedNumber + " " + standardValue + " " + chanceNumber); test part
+        int result;
+        if (preResult < chanceNumber) { // the positive result for the player
+            //System.out.println(result); test part
+            System.out.println("the opponent died"); //test part
+            return result = 1;
+        } else {
+            result = 0; // the negativ result for the player
+            //System.out.println(result); test part
+            System.out.println("the opponent lived"); //test part
+            return result;
+        }
+
+
+    }
+
+    public static double calculateInjuring(double chanceNumber, double standardValue, double generatedNumber) {
+        double preResult = generatedNumber / standardValue;
+        System.out.println("generated: " + generatedNumber + " " + standardValue + " " + chanceNumber);
+        int result;
+        if (preResult < chanceNumber) { // the positive result for the player
+            result = 1;
+            System.out.println(result);
+            System.out.println("the opponent was injured");
+            return result;
+        } else {
+            result = 0; //the negativ result for the player
+            System.out.println(result);
+            System.out.println("the action failed");
+            return result;
+        }
+
+    }
 }
+
--- Src/Game/Main.java
+++ Src/Game/Main.java
@@ -9,6 +9,25 @@ public class Main {
         Player kvothe = new Player();
         kvothe.setHealth(20);
 
+        kvothe.setNumberForChances(2);
+        kvothe.setChanceOfKilling(0.1);
+        kvothe.setChanceOfInjuring(0.2);
+        kvothe.setChanceOfSubduing(0.12);
+        kvothe.setChanceOfWinningOver(0.01);
+       double killingResult=Calculation.calculateKilling(kvothe.getChanceOfKilling(), kvothe.getNumberForChances(),  Calculation.generateRandomInt());
+        //killingResult returns 1 or 0 of result in Calculation.  so 1= opponent died, 0= opponents survived about 1/5 within 1000 tries dies
+
+            int tally=0;
+        for (int i =0; i<1000; i++) {
+            double injuriedResult = Calculation.calculateInjuring(kvothe.getChanceOfInjuring(), kvothe.getNumberForChances(),Calculation.generateRandomInt());
+
+            if (injuriedResult==1.0) {
+                tally++;
+                System.out.println(tally);
+            }
+
+        }
+
 
 
     }
